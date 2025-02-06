c9862414a8f25dfcab91e40c8998bd48f6df45f0
Thalia Danielle <thaliadani2@gmail.com>
2025-02-06 10:22:23 -0300

    Add files via upload

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,28 +1,25 @@
-function enviarFormulario() {
-  const resultadosDiv = document.getElementById("resultados");
-  resultadosDiv.innerHTML = "";
+function validarLogin() {
+  let usuario = document.getElementById('usuario').value;
+  let senha = document.getElementById('senha').value;
 
-  const nome = document.getElementById("nome").value;
-  const senha = document.getElementById("senha").value;
+  // '/' Indica o inicio e o fim da operação
+  // '^' Indica o inicio da string
+  // '$' Indica o fim da string
+  // 'test' retorna verdadeiro ou falso
+  let validarUsuario = /^[13579]{5}$/.test(usuario);
+  let validarSenha = /^[02468]{4}$/.test(senha);
 
-  if ((nome, senha)) {
-    sessionStorage.setItem("nome", nome);
-    sessionStorage.setItem("senha", senha);
-    window.location.href = "./dados-do-cadastro.html";
+  if (validarUsuario && validarSenha) {
+    alert('Usuário e senha válidos.');
   } else {
-    alert("Digitou o nome ou a senha errado");
+    //'\n' Pula uma linha do texto no javascript
+    alert('Usuário ou senha inválidos!\n\n Lembre-se:\n- O usuário precisa conter apenas 5 dígitos com apenas números ímpares.\n- A senha precisa ser de apenas 4 dígitos com números pares.');
   }
 }
 
-const nome = sessionStorage.getItem("nome");
-const senha = sessionStorage.getItem("senha");
-
-if (nome && senha) {
-  document.getElementById("dados").innerHTML = `
-    <p>> <strong>Nome:</strong> ${nome}</p>
-    <p>> <strong>Senha:</strong> ${senha}</p>
-    `;
-}else{
-    document.getElementById('dados').innerHTML = `
-    <p>Dados não encontrados</p>`
+// Digitar no input apenas numeros
+function apenasNumeros(event) {
+  const input = event.target;
+  input.value = input.value.replace(/[^0-9]/g, '');
 }
+
